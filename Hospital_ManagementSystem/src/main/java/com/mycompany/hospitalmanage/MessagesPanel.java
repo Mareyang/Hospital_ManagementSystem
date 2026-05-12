901a341ef4fee5ec09a4b8558f6b10315417a823
Mareyang <marietheadelacruz@gmail.com>
2026-05-12 10:49:18 +0800

    staffs

--- Hospital_ManagementSystem/src/main/java/com/mycompany/hospitalmanage/MessagesPanel.java
+++ Hospital_ManagementSystem/src/main/java/com/mycompany/hospitalmanage/MessagesPanel.java
@@ -49,5 +49,10 @@ public class MessagesPanel extends JPanel {
         btnAdd.setForeground(ColorsTheme.Text_White);
         add(btnAdd);
         
+        
+        
+        
+        
+        
     }   
 }
